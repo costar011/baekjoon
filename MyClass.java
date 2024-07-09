c398c281a5e37eef3c9d410613ca4033635c896e
costar011 <dnaer456@gmail.com>
2024-07-09 18:01:14 +0900

    10926번푸는중

---
+++ MyClass.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ * 백준 10926번 문제
+ * 
+ */
+
+public class MyClass {
+    String  A = "??!";
+}
---
+++ 브론즈/2557.c
@@ -0,0 +1,15 @@
+/*
+문제
+
+Hello World!를 출력하시오.
+
+출력
+Hello World!를 출력하시오.
+*/
+
+#include <stdio.h>
+
+int main(void){
+printf("Hello World!");
+    return 0;
+}
