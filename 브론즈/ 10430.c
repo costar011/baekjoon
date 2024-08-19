0ceae7b8ddce0fe56b45787a31d30e696e6e75fa
costar011 <dnaer456@gmail.com>
2024-08-20 07:38:05 +0900

    10430

--- 브론즈/ 10430.c
+++ 브론즈/ 10430.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    return 0;
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%d\n", (a + b) % c);
+	printf("%d\n", (a % c + b % c) % c);
+	printf("%d\n", (a * b) % c);
+	printf("%d\n", (a % c * b % c) % c);
+	return 0;
 }
